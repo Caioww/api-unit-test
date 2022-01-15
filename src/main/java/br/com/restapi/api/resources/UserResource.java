56502548c4d5086c34aa95b1daf2aaff2bfd5cd7
Caioww <sousinhacaio@gmail.com>
2022-01-15 17:25:01 -0300

    criando exceptions e incluindo modelmapper

--- src/main/java/br/com/restapi/api/resources/UserResource.java
+++ src/main/java/br/com/restapi/api/resources/UserResource.java
@@ -1,5 +1,6 @@
 package br.com.restapi.api.resources;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,19 +8,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.restapi.api.domain.User;
+import br.com.restapi.api.domain.dto.UserDto;
 import br.com.restapi.api.services.UserService;
 
 @RestController
 @RequestMapping(value="/user")
 public class UserResource {
 	
+	@Autowired 
+	private ModelMapper mapper;
+	
 	@Autowired
 	private UserService service;
 	
 	@GetMapping(value="/{id}")
-	public ResponseEntity<User> findById(@PathVariable Integer id){
-		return ResponseEntity.ok().body(service.findById(id));
+	public ResponseEntity<UserDto> findById(@PathVariable Integer id){
+		return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDto.class));
 		
 	}
 
--- src/main/java/br/com/restapi/api/services/UserServiceImpl.java
+++ src/main/java/br/com/restapi/api/services/UserServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.restapi.api.domain.User;
+import br.com.restapi.api.exceptions.ObjectNotFoundException;
 import br.com.restapi.api.repositories.UserRepository;
 
 @Service
@@ -18,7 +19,7 @@ public class UserServiceImpl implements UserService{
 	public User findById(Integer id) {
 		Optional<User> obj = repository.findById(id);
 		
-		return obj.orElse(null);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
 		
 	}
 
